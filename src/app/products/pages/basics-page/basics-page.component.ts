5a41baf269420f2ffe8b0aaca59ee7ab58a0261f
OrlandoYV <sergioyv@gmail.com>
2024-07-15 23:37:06 -0500

    oy - uppercase, lowercase pipes

--- src/app/products/pages/basics-page/basics-page.component.ts
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-basics-page',
   templateUrl: './basics-page.component.html',
-  styleUrl: './basics-page.component.css'
+  styleUrl: './basics-page.component.css',
 })
 export class BasicsPageComponent {
-
+  public nameLowerCase: string = 'orlando';
+  public nameUpperCase: string = 'ORLANDO';
+  public fullname: string = 'OrlAndO sErgIo';
 }
