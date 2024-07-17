f7d3ca87f56de55accbf2a4e842c2cc3b1c637f5
OrlandoYV <sergioyv@gmail.com>
2024-07-17 12:47:17 -0500

    oy - locale language

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+//configuration of locale (language)
+import localeEsPE from '@angular/common/locales/es-PE';
+import localeDeutsch from '@angular/common/locales/de';
+import { registerLocaleData } from '@angular/common';
 
+registerLocaleData(localeEsPE)
+registerLocaleData(localeDeutsch);
 @NgModule({
   declarations: [AppComponent],
   imports: [
